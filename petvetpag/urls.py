3b337c0c8433213ec6a77d3a2aba6803fb943dac
SalitaaDuoc <al.salasi@duocuc.cl>
2024-06-30 07:06:12 -0400

    avances

--- petvetpag/urls.py
+++ petvetpag/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('porhacer/', views.porhacer, name='porhacer'),
     path('tienda/', views.tienda, name='tienda'),
+
+    #crud para tienda  
+    path('tienda/create/', views.product_create, name='product_create'),
+    #path('tienda/update/<int:id>/', views.product_update, name='product_update'),
+    #path('tienda/delete/<int:id>/', views.product_delete, name='product_delete'),
+
 ]
--- petvetpag/views.py
+++ petvetpag/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import product
+from .forms import productForm
 
 # Create your views here.
 def index(request):
@@ -10,7 +11,39 @@ def porhacer(request):
     context = {}
     return render(request, 'petvetpag/porhacer.html')
 
-def tienda(request):
+#Crud productos (Tienda)
+def tienda(request, context={}):
     productos = product.objects.all()
-    context = {'productos': productos}
+    context = {
+        'productos': productos,
+        'form': productForm()}
     return render(request, 'petvetpag/tienda.html', context)
+
+def product_create(request):
+    context = {}
+
+    if request.method == 'POST':
+        print('----------------------------Se llego a post')
+        form = productForm(request.POST, request.FILES)
+        if form.is_valid():
+            print('---------------------------Formulario valido')
+            form.save()
+            form = productForm()
+            context['message'] = "Guardado exitosamente"
+            context['form'] = form
+        else:
+            print('---------------------------Formulario invalido'+str(form.errors))
+            context['message'] = "Formulario invalido"
+    else:
+        print('---------------------------Se llego a get')
+        context['message'] = "No se ha registrado ningun producto"
+
+    return tienda(request, context)
+
+def product_update(request, id):
+    context = {}
+    return tienda(request, context)
+
+def product_delete(request, id):
+    context = {}
+    return tienda(request, context)
